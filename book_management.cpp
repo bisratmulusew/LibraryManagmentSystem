ec9aa787805fb206763eb79e903de21c813ae6e8
bisratmulusew <bisratmulusew@gmail.com>
2025-05-23 20:25:38 +0300

    ✅ Implemented Book Category Managment

---
+++ book_management.cpp
@@ -0,0 +1,7 @@
+#include "book_management.h"
+#include <iostream>
+using namespace std;
+
+void BookCategoryManagment() {
+    cout << "Book category managment functionality\n";
+}
---
+++ book_management.h
@@ -0,0 +1,7 @@
+#ifndef BOOK_MANAGEMENT_H
+#define BOOK_MANAGEMENT_H
+
+void BookCategoryManagment();
+
+
+#endif
