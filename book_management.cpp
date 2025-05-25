f133f45dcb586ecb6ced85a5f61daa4c663ce360
bisratmulusew <bisratmulusew@gmail.com>
2025-05-25 14:16:34 +0300

    🛠️ Improved Book Category logic

--- book_management.cpp
+++ book_management.cpp
@@ -1,7 +1,15 @@
 #include "book_management.h"
 #include <iostream>
+#include <algorithm> 
 using namespace std;
 
-void BookCategoryManagment() {
-    cout << "Book category managment functionality\n";
+book::book(string t, string g) {
+    title = t;
+    genre = g;
+}
+
+void BookCategoryManagment(vector<book>& books) {
+    sort(books.begin(), books.end(), [](const book& a, const  book& b) {
+        return a.genre < b.genre;
+    });
 }
--- book_management.h
+++ book_management.h
@@ -1,7 +1,18 @@
 #ifndef BOOK_MANAGEMENT_H
 #define BOOK_MANAGEMENT_H
 
-void BookCategoryManagment();
+#include <string>
+#include <vector>
+using namespace std;
+
+class book {
+public:
+    string title;
+    string genre;
+
+    book(string t, string g);
+};
+void BookCategoryManagment(vector<book>& books);
 
 
 #endif
