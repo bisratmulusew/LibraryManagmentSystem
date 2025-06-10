a6d037c56333b0b7e48dfac5707d84bf9051e809
becs-maker <rebekaeshete@gmail.com>
2025-06-10 18:38:27 +0300

    Update book.h

--- book.h
+++ book.h
@@ -2,25 +2,25 @@
 #define BOOK_H
 
 #include <string>
-
 using namespace std;
 
-class Book {
-private:
-    string isbn, title, author, genre;
-    int quantity;
+enum Genre {
+    FICTION,
+    NON_FICTION,
+    MYSTERY,
+    SCI_FI,
+    BIOGRAPHY,
+    OTHER
+};
 
-public:
-    Book();
-    Book(string, string, string, string, int);
+struct Book {
+    string title;
+    string author;
+    int year;
+    Genre genre;
 
     void inputBookDetails();
-    void displayBook() const;
-    string getISBN() const;
-    void editBookDetails();
-
     string toText() const;
-    static Book fromText(const string&);
 };
 
 #endif
