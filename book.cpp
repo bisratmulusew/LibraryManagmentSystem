5570ddbf1057a3d7f49e2dc5d06d6235ac4c9bfd
becs-maker <rebekaeshete@gmail.com>
2025-06-10 18:37:51 +0300

    Update book.cpp

--- book.cpp
+++ book.cpp
@@ -1,55 +1,29 @@
 #include "Book.h"
 #include <iostream>
-#include <sstream>
-
 using namespace std;
 
-Book::Book() {}
-
-Book::Book(string isbn, string title, string author, string genre, int quantity)
-    : isbn(isbn), title(title), author(author), genre(genre), quantity(quantity) {}
-
 void Book::inputBookDetails() {
-    cout << "Enter ISBN: ";
-    getline(cin, isbn);
-    cout << "Enter Title: ";
+    int genreChoice;
+    cout << "Enter book title: ";
     getline(cin, title);
-    cout << "Enter Author: ";
+    cout << "Enter author name: ";
     getline(cin, author);
-    cout << "Enter Genre: ";
-    getline(cin, genre);
-    cout << "Enter Quantity: ";
-    cin >> quantity;
-    cin.ignore();  // clear newline
-}
-
-void Book::displayBook() const {
-    cout << "ISBN: " << isbn << "\nTitle: " << title << "\nAuthor: " << author
-         << "\nGenre: " << genre << "\nQuantity: " << quantity << "\n";
-}
-
-string Book::getISBN() const {
-    return isbn;
-}
-
-void Book::editBookDetails() {
-    cout << "Editing Book...\n";
-    inputBookDetails();
+    cout << "Enter year of publication: ";
+    cin >> year;
+    cout << "Select genre:\n";
+    cout << "0 - Fiction\n";
+    cout << "1 - Non-Fiction\n";
+    cout << "2 - Mystery\n";
+    cout << "3 - Sci-Fi\n";
+    cout << "4 - Biography\n";
+    cout << "5 - Other\n";
+    cout << "Choice: ";
+    cin >> genreChoice;
+    cin.ignore(); // Clear newline from input buffer
+    genre = static_cast<Genre>(genreChoice);
 }
 
 string Book::toText() const {
-    return isbn + "," + title + "," + author + "," + genre + "," + to_string(quantity);
-}
-
-Book Book::fromText(const string& line) {
-    stringstream ss(line);
-    string token;
-    string fields[5];
-    int i = 0;
-
-    while (getline(ss, token, ',') && i < 5) {
-        fields[i++] = token;
-    }
-
-    return Book(fields[0], fields[1], fields[2], fields[3], stoi(fields[4]));
+    string genreStr[] = { "Fiction", "Non-Fiction", "Mystery", "Sci-Fi", "Biography", "Other" };
+    return title + "," + author + "," + to_string(year) + "," + genreStr[genre];
 }
