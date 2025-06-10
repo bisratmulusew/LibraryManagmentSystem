51d90ab41a5ee7b1b0f6e9d88a24967eb92b6828
bisratmulusew <bisratmulusew@gmail.com>
2025-06-10 21:25:50 +0300

    WIP: Save changes before rebase

---
+++ book managment module/book_management.h
@@ -0,0 +1,48 @@
+#ifndef BOOK_MANAGEMENT_H
+#define BOOK_MANAGEMENT_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum class BookStatus {
+    Available,
+    Borrowed,
+    Lost
+};
+
+
+enum Genre {
+    FICTION,
+    NONFICTION,
+    SCIENCE,
+    HISTORY,
+    OTHER,
+    GENRE_COUNT  
+};
+
+class Book {
+private:
+    string title;
+    string author;
+    BookStatus status;
+    Genre genre;
+
+public:
+    Book(const string& title, const string& author, Genre genre);
+    
+    void borrow();
+    void returnBook();
+    void markAsLost();
+    
+    BookStatus getStatus() const;
+    string getTitle() const;
+    string getAuthor() const;
+    Genre getGenre() const;
+};
+
+void BookCategoryManagement(const vector<Book>& books);
+string genreToString(Genre genre);
+
+#endif // BOOK_MANAGEMENT_H
