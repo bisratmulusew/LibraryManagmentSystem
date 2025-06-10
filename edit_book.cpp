eda6167050efe20a00ffc696f78b67184f7f2092
becs-maker <rebekaeshete@gmail.com>
2025-06-10 19:00:00 +0300

    Update edit_book.cpp

--- edit_book.cpp
+++ edit_book.cpp
@@ -5,35 +5,55 @@
 
 using namespace std;
 
+// Load all books from file
 vector<Book> loadBooks(const string& filename) {
-    ifstream file(filename);
     vector<Book> books;
+    ifstream file(filename);
     string line;
 
+    if (!file) {
+        cerr << "Error opening file for reading.\n";
+        return books;
+    }
+
     while (getline(file, line)) {
-        books.push_back(Book::fromText(line));
+        books.push_back(fromText(line));
     }
 
     return books;
 }
 
+// Save all books back to file
 void saveBooks(const vector<Book>& books, const string& filename) {
     ofstream file(filename);
+    if (!file) {
+        cerr << "Error opening file for writing.\n";
+        return;
+    }
+
     for (const Book& book : books) {
-        file << book.toText() << "\n";
+        file << toText(book) << "\n";
     }
 }
 
+// Edit a book identified by ISBN
 void editBook(const string& filename) {
     vector<Book> books = loadBooks(filename);
-    string isbn;
+
+    if (books.empty()) {
+        cout << "No books found.\n";
+        return;
+    }
+
+    string isbnToEdit;
     cout << "Enter ISBN of the book to edit: ";
-    getline(cin, isbn);
+    getline(cin, isbnToEdit);
 
     bool found = false;
+
     for (Book& book : books) {
-        if (book.getISBN() == isbn) {
-            book.editBookDetails();
+        if (book.isbn == isbnToEdit) {
+            editBookDetails(book);  // Uses Book.cpp logic
             found = true;
             break;
         }
@@ -43,7 +63,7 @@ void editBook(const string& filename) {
         saveBooks(books, filename);
         cout << "Book edited successfully.\n";
     } else {
-        cout << "Book not found.\n";
+        cout << "Book with ISBN '" << isbnToEdit << "' not found.\n";
     }
 }
 
